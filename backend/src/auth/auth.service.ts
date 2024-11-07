b7a7e19bf47ddefe90ed8389aacc66ed7c87bf97
gabhack <gabhack@gmail.com>
2024-11-07 11:18:56 -0800

    back con verificacion jwt (sin generacion)

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
@@ -8,31 +7,32 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService,
   ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findUserByEmail(email);
     if (user && await bcrypt.compare(pass, user.password)) {
       const { password, ...result } = user;
-      return result;
+      return result;  
     }
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+  async login(email: string, password: string) {
+    const user = await this.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return { success: true };  
   }
 
   async register(data: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(data.password, 10);
     return this.usersService.createUser({
-        ...data, password: hashedPassword,
-        dni: '',
-        userType: ''
+      ...data, 
+      password: hashedPassword,
+      dni: '',
+      userType: ''
     });
   }
 }
