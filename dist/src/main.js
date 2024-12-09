9d6827d1db5aa79680fc2e3c8eb0e8330a9dd4ba
gabhack <gabhack@gmail.com>
2024-12-09 14:15:33 -0500

    fixed endpoint payments

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,15 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const express = require("express");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { bodyParser: false });
     app.use('/payment/webhook', express.raw({ type: 'application/json' }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.enableCors();
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Documentación de la API')
+        .setDescription('Documentación de la API para la aplicación')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api-docs', app, document);
     await app.listen(3001);
-    console.log(`Aplicación escuchando en http://localhost:3001`);
+    console.log('Aplicación escuchando en http://localhost:3001');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
-import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 
 async function bootstrap() {
+  // Inicializa la aplicación
   const app = await NestFactory.create(AppModule, { bodyParser: false });
 
-  // Deshabilitar bodyParser solo para el webhook
+  // Configuración del middleware raw solo para el webhook
   app.use('/payment/webhook', express.raw({ type: 'application/json' }));
 
-  // Habilitar bodyParser para otros endpoints
+  // Habilitar bodyParser para el resto de los endpoints
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  // Habilitar CORS
+  app.enableCors();
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Documentación de la API')
+    .setDescription('Documentación de la API para la aplicación')
+    .setVersion('1.0')
+    .addBearerAuth() // Agregar autenticación Bearer para JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // Iniciar el servidor
   await app.listen(3001);
-  console.log(`Aplicación escuchando en http://localhost:3001`);
+  console.log('Aplicación escuchando en http://localhost:3001');
 }
+
 bootstrap();
